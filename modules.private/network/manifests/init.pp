0484628c3f775c085abd18075c793eca772529dd
Lennart Betz <lennart.betz@netways.de>
2016-07-17 14:45:38 +0200

    fix missing parent in agent zones, remove selinux management

--- modules.private/network/manifests/init.pp
+++ modules.private/network/manifests/init.pp
@@ -7,16 +7,16 @@ class network($dns = 'yes') {
         enable => false,
       }
 
-      exec { 'selinux-disable':
-        path    => '/usr/sbin:/usr/bin',
-        command => 'setenforce 0',
-        unless  => 'getenforce |/bin/grep -n Permissive 2>&1>/dev/null',
-      }
-      file_line { 'selinux-disable':
-        path   => '/etc/sysconfig/selinux',
-        line  => 'SELINUX=permissive',
-        match => '^SELINUX=',
-      }
+      #exec { 'selinux-disable':
+      #  path    => '/usr/sbin:/usr/bin',
+      #  command => 'setenforce 0',
+      #  unless  => 'getenforce |/bin/grep -n Permissive 2>&1>/dev/null',
+      #}
+      #file_line { 'selinux-disable':
+      #  path   => '/etc/sysconfig/selinux',
+      #  line  => 'SELINUX=permissive',
+      #  match => '^SELINUX=',
+      #}
 
       file_line { 'peerdns':
         path   => '/etc/sysconfig/network-scripts/ifcfg-enp0s3',
@@ -31,10 +31,12 @@ class network($dns = 'yes') {
     'windows': {
       exec {"Disable-Firewall-Profile-private":
         command  => 'Set-NetFirewallProfile -Profile private -Enabled False',
+        onlyif   => 'if (!(Get-NetFirewallProfile -Profile private).Enabled) { exit 1 }',
         provider => powershell,
       }
       exec {"Disable-Firewall-Profile-public":
         command  => 'Set-NetFirewallProfile -Profile public -Enabled False',
+        onlyif   => 'if (!(Get-NetFirewallProfile -Profile public).Enabled) { exit 1 }',
         provider => powershell,
       }
     }

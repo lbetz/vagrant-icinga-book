4949337ef9632e6e5cc36e2652dce531fe3565a6
Lennart Betz <lennart.betz@netways.de>
2016-11-23 19:00:50 +0100

    add updates and mssql monitoring

--- modules.private/profile/manifests/ads.pp
+++ modules.private/profile/manifests/ads.pp
@@ -18,6 +18,12 @@ class profile::ads {
     source             => 'puppet:///modules/profile/icinga2/scripts/check_ad.vbs',
     require            => Class['nscp'],
   }
+  file { 'C:/Program Files/NSClient++/scripts/custom/check_windows_updates.ps1':
+    ensure             => file,
+    source_permissions => ignore,
+    source             => 'puppet:///modules/profile/icinga2/scripts/check_windows_updates.ps1',
+    require            => Class['nscp'],
+  }
 
   class { 'windows_ad':
     install                => present,

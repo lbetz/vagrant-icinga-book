7d46a55efc4e918ed85acb4428ea64ce3ed4793b
Lennart Betz <lennart.betz@netways.de>
2016-11-24 21:01:01 +0100

    fix dependency cycle - nscp

--- modules.private/profile/manifests/nscp.pp
+++ modules.private/profile/manifests/nscp.pp
@@ -1,26 +1,23 @@
 class profile::nscp {
 
-  class { 'nscp':
+  class { '::nscp':
     ensure => running,
     enable => true,
   }
 
-  file { 'C:/Program Files/NSClient++/scripts/custom/check_time.vbs':
+  -> file { 'C:/Program Files/NSClient++/scripts/custom/check_time.vbs':
     ensure             => file,
     source_permissions => ignore,
     source             => 'puppet:///modules/profile/icinga2/scripts/check_time.vbs',
-    require            => Class['nscp'],
   }
-  file { 'C:/Program Files/NSClient++/scripts/custom/check_ad.vbs':
+  -> file { 'C:/Program Files/NSClient++/scripts/custom/check_ad.vbs':
     ensure             => file,
     source_permissions => ignore,
     source             => 'puppet:///modules/profile/icinga2/scripts/check_ad.vbs',
-    require            => Class['nscp'],
   }
-  file { 'C:/Program Files/NSClient++/scripts/custom/check_windows_updates.ps1':
+  -> file { 'C:/Program Files/NSClient++/scripts/custom/check_windows_updates.ps1':
     ensure             => file,
     source_permissions => ignore,
     source             => 'puppet:///modules/profile/icinga2/scripts/check_windows_updates.ps1',
-    require            => Class['nscp'],
   }
 }

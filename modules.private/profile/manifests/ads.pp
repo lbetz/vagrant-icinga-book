34ee1d357752b487e41737cdad1d8177d37245c3
Lennart Betz <lennart.betz@netways.de>
2016-11-24 20:31:37 +0100

    add profile for nsclient

--- modules.private/profile/manifests/ads.pp
+++ modules.private/profile/manifests/ads.pp
@@ -1,30 +1,6 @@
 class profile::ads {
   include profile::base
 
-  class { 'nscp':
-    ensure => running,
-    enable => true,
-  }
-
-  file { 'C:/Program Files/NSClient++/scripts/custom/check_time.vbs':
-    ensure             => file,
-    source_permissions => ignore,
-    source             => 'puppet:///modules/profile/icinga2/scripts/check_time.vbs',
-    require            => Class['nscp'],
-  }
-  file { 'C:/Program Files/NSClient++/scripts/custom/check_ad.vbs':
-    ensure             => file,
-    source_permissions => ignore,
-    source             => 'puppet:///modules/profile/icinga2/scripts/check_ad.vbs',
-    require            => Class['nscp'],
-  }
-  file { 'C:/Program Files/NSClient++/scripts/custom/check_windows_updates.ps1':
-    ensure             => file,
-    source_permissions => ignore,
-    source             => 'puppet:///modules/profile/icinga2/scripts/check_windows_updates.ps1',
-    require            => Class['nscp'],
-  }
-
   class { 'windows_ad':
     install                => present,
     installmanagementtools => true,
--- modules.private/role/manifests/ads.pp
+++ modules.private/role/manifests/ads.pp
@@ -1,3 +1,4 @@
 class role::ads {
   include profile::ads
+  include profile::nscp
 }

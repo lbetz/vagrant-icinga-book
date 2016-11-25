9e94bb8a076cac0334493b5beef195fb2b202177
Lennart Betz <lennart.betz@netways.de>
2016-11-25 11:05:28 +0100

    remove puppet agent from host gmw

--- modules.private/role/manifests/groupware.pp
+++ modules.private/role/manifests/groupware.pp
@@ -1,6 +1,5 @@
 class role::groupware {
   include profile::base
-  include profile::puppet::agent
   include profile::ldap::server
   include profile::postfix::groupware
   include profile::icinga2::agent

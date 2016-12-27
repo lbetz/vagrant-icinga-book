c595594dc76cec65b2784abe40d8154c707eed3c
Lennart Betz <lennart.betz@netways.de>
2016-12-27 08:33:38 +0100

    cleanup shell provisioning

--- scripts/pre-install.sh
+++ scripts/pre-install.sh
@@ -1,5 +1,3 @@
-#rpm --import https://yum.puppetlabs.com/RPM-GPG-KEY-puppetlabs
 rpm --import https://yum.puppetlabs.com/RPM-GPG-KEY-puppet
-rpm -aq |grep puppetlabs-release 1>/dev/null || yum install -y https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm #https://yum.puppetlabs.com/el/7/products/x86_64/puppetlabs-release-7-12.noarch.rpm
-
+rpm -aq |grep puppetlabs-release 1>/dev/null || yum install -y https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
 rpm -q puppet-agent || yum install -y puppet-agent
